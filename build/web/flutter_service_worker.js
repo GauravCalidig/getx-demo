95881fe278947ff4b74cbae4661e91f0769a26c0
Gaurav.Calidig <gaurav.yadav@calidig.com>
2021-09-07 17:25:38 +0530

    minor

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -10,9 +10,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "8a8fdb6a6615e812f52c4f60bbb44e96",
-"/": "8a8fdb6a6615e812f52c4f60bbb44e96",
-"main.dart.js": "8008dc776a86dbd2f4af38d900ecdb64",
+"index.html": "9a3168b37d704cb984be16bc7ee7062f",
+"/": "9a3168b37d704cb984be16bc7ee7062f",
+"main.dart.js": "11d205ceb1171270ab23e45682d2b59b",
 "manifest.json": "c25526e4d612eb810b5927d2cfc9ddb0",
 "version.json": "34b58d8feb7b22d053ce925752b6d088"
 };
